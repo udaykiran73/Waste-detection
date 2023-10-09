50e4466b41107df9bd5d9b2b3952af5f9f7cf9b7
udaykiran73 <143659823+udaykiran73@users.noreply.github.com>
2023-10-09 19:13:02 -0400

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Uday kiran Alimi',
+    author_email= 'udaykirancoys@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
